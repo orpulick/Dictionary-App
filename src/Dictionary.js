59a27f74fefd0e7f2a4a18871f98f9706f70d005
orpulick <o_pulick@yahoo.com>
2022-06-12 13:16:29 -0600

    added dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -10,7 +14,8 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
