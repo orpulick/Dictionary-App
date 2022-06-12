28f5325d014f0c704746b75e38a59fd01a998284
orpulick <o_pulick@yahoo.com>
2022-06-12 16:23:07 -0600

    added meaning to the word with examples

---
+++ src/Meaning.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h5>{props.meaning.partOfSpeech}</h5>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              <strong>Definition: </strong>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
