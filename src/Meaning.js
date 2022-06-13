55bfaf2ba2be6d9cfc327b86ff88d9798490fdd7
orpulick <o_pulick@yahoo.com>
2022-06-13 14:42:47 -0600

    Added Synonyms to the dictionary app

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -13,6 +14,8 @@ export default function Meaning(props) {
               {definition.definition}
               <br />
               <em>{definition.example}</em>
+              <strong>Synonyms: </strong>{" "}
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
